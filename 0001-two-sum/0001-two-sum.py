0df8d423622698b6e18a462f3cf317d3a6375c70
Mustapha Adams <adams.mustapha.dev@gmail.com>
2026-09-23 14:01:53 +0100

    Time: 0 ms (100%), Space: 20.3 MB (59.51%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -3,8 +3,14 @@ class Solution:
 
         N = len(nums)
 
+        hmap = {}
+
         for i in range(N):
-            for j in range(i + 1, N):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+            diff = target - nums[i] 
+            if diff in hmap:
+                return [i, hmap[diff]]
+
+            else:
+                hmap[nums[i]] = i
+                
         
